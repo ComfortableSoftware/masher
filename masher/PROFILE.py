ea5f10d6cac1a760d65e3240790f91907c87cdf9
GaelicGrime <will.angus.blaylock@gmail.com>
2024-02-22 05:47:37 +0000

    20240221_224737

--- masher/PROFILE.py
+++ masher/PROFILE.py
@@ -1,9 +1,19 @@
 
 
+import KEYS
+locals().update(KEYS.ALL_THE_KEYS)
+
+
 DEFAULT_PROFILE = {
-  "DEFT":
+  KD_CONNEXION:  # Connexion 3D mouse
     {},
-  "MIMD":
+  KD_DEFT:  # DEFT mouse
+    {},
+  KD_POWERMATE:
+    {},
+  KD_SAITEK:  # Saitek Cyborg
+    {},
+  KD_SHANWAN:  # MIMD game controller
     {},
 }
 
